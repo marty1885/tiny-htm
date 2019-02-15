a4a3a23d580c58546adfbb636fe6abba1abbc383
marty1885 <marty188586@gmail.com>
2019-02-15 18:26:28 +0800

    more improvments to tmtest

--- examples/tmtest.cpp
+++ examples/tmtest.cpp
@@ -12,12 +12,25 @@
 
 int main()
 {
-	TemporalMemory tm({8}, 6);
+	//In this program, TM is going to learn the pattern of 01010101.. So we have 2 possible inputs, 1 and 0,
+	//Trough arbitrary decition, each input has 4 bits.
 	CategoryEncoder encoder(2, 4);
+	TemporalMemory tm({8}, 6); //So the TM has to accept 8 bits of input data. A column size of 6 is arbitrary.
+
 	std::cout << "Learning the sequence 01010101...." << std::endl << std::endl
-		<< "Algorithm prediction (N = no prediction): " << std::endl;
+		<< "Ground truth and algorithm prediction (N = no prediction): " << std::endl;
+
+	//Print the ground truth vales
+	for(int i=0;i<40 ;i++)
+		std::cout << (i+1)%2;
+	std::cout << std::endl;
+
+	//Let the TM learn the patten on the go and make predictions
 	for(int i=0;i<40 ;i++) {
+		//Learn the pattern and make prediction (true = enable learning)
 		auto res = tm.compute(encoder.encode(i%2), true);
+
+		//Decode the results. Print 'N' in gray if no prediction
 		auto cat = encoder.decode(res);
 		if(cat.size() == 0)
 			std::cout << "\033[90mN\033[0m";
